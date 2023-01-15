2d71b5a6685c6b27783b56600c6cb2b0dc58b6ff
yvgo <yvonne.goetzeler@gmail.com>
2023-01-15 15:00:12 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function render() {
+    let content = document.getElementById('content'); //rendern heißt html code erstellen
+    content.innerHTML = '';
+    content.innerHTML += `<h1>Überschrift 1</h1>`;
+}
